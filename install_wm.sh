419f073deb5e3036bf63d3ed3d8dd2a724ef18ec
GraeWolf <kmccuddy@startmail.com>
2025-12-08 19:42:46 -0500

    added --noconfirm --needed flags to the first pacman and removed pulseaudio package

--- install_wm.sh
+++ install_wm.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 # Install Bspwm, polybar and helper apps.
-sudo pacman -S bspwm sxhkd rofi picom polybar fastfetch arandr\
-           polkit-gnome gvfs udisks2 udiskie networkmanager \
+sudo pacman -S --noconfirm --needed bspwm sxhkd rofi picom polybar fastfetch \
+           arandr polkit-gnome gvfs udisks2 udiskie networkmanager \
            network-manager-applet nm-connection-editor \
-           bluez bluez-utils pulseaudio pulseaudio-alsa pavucontrol \
+           bluez bluez-utils pulseaudio-alsa pavucontrol \
            lxappearance lxsession feh unzip unzip alsa-utils
 
 chmod 755 bspwm/bspwmrc
