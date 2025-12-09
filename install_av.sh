3be2dd788f51a9e16c4b046d7ccfe4c2f5d89c6c
GraeWolf <kmccuddy@startmail.com>
2025-12-09 11:47:22 -0500

    Initial commit

---
+++ install_av.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+sudo pacman -S --noconfirm --needed mpv ffmpeg gstreamer gst-libav \
+	gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly \
+	gst-plugin-pipewire 
