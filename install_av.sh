7d443b6ea4aa0256228202484c15e789f5afca0f
GraeWolf <kmccuddy@startmail.com>
2025-12-09 11:49:58 -0500

    only deleted a blank space

--- install_av.sh
+++ install_av.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 sudo pacman -S --noconfirm --needed mpv ffmpeg gstreamer gst-libav \
 	gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly \
-	gst-plugin-pipewire 
+	gst-plugin-pipewire
