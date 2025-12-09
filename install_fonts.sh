3244dc4bc5b093dfff011e4bd2e42625ff0af704
GraeWolf <kmccuddy@startmail.com>
2025-12-08 20:21:40 -0500

    fixed typo of missing t in xdg-utils

--- install_fonts.sh
+++ install_fonts.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 # installing fonts
 sudo pacman -S --noconfirm --needed ttf-dejavu noto-fonts ttf-cascadia-code-nerd \
 	ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols \
-	fontconfig xdg-uils
+	fontconfig xdg-utils
