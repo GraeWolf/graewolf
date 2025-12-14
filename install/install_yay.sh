89fdd038c1764455d4ee3e805b19fb861166adf3
GraeWolf <kmccuddy@startmail.com>
2025-12-13 19:16:52 -0500

    closed the if statment and add --noconfirm to makpkg

--- install/install_yay.sh
+++ install/install_yay.sh
@@ -5,10 +5,11 @@ set -euo pipefail
 git clone https://aur.archlinux.org/yay.git $(pwd)/yay
 
 cd $(pwd)/yay
-makepkg -si
+makepkg -si --noconfirm
 
 # Make sure yay is installed
 if command -v yay >/dev/null 2>&1; then
 	rm -rf $(pwd)/yay
 else
 	echo "Yay did not install."
+fi
