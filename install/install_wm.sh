e388625c0b33acddfb64df2a527cd715c6dec1c0
GraeWolf <kmccuddy@startmail.com>
2025-12-13 22:57:17 -0500

    added xsettingsd to pacman command

--- install/install_wm.sh
+++ install/install_wm.sh
@@ -8,7 +8,7 @@ sudo pacman -S --noconfirm --needed bspwm sxhkd rofi picom polybar fastfetch \
            network-manager-applet nm-connection-editor \
            bluez bluez-utils pulseaudio-alsa pavucontrol \
            lxappearance lxsession feh unzip unzip alsa-utils eza bat \
-		   pcmanfm
+		   pcmanfm xsettingsd
 
 chmod 755 bspwm/bspwmrc
 chmod 644 sxhkd/sxhkdrc
