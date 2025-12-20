e39d313dee017453b420be594c6d3b16666d3cd7
GraeWolf <kmccuddy@startmail.com>
2025-12-20 13:51:09 -0500

    added symlink for xsettings

--- install/install_wm.sh
+++ install/install_wm.sh
@@ -14,6 +14,7 @@ chmod 755 bspwm/bspwmrc
 chmod 644 sxhkd/sxhkdrc
 
 ln -sfn $(pwd)/bspwm /home/$USER/.config/bspwm
+ln -sfn $(pwd)/xsettingsd /home/$USER/.config/xsettingsd
 ln -sfn $(pwd)/picom /home/$USER/.config/picom
 ln -sfn $(pwd)/polybar /home/$USER/.config/polybar
 ln -sfn $(pwd)/sxhkd /home/$USER/.config/sxhkd
